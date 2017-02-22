4021d98c3047cfefeaed8f61226cfe37a1660800
klounErik <janegge1990@gmail.com>
2017-02-22 06:34:55 +0100

    added sections for the page

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-function showmsg(){
-    
-    $(".contact").toggle();
-    }
+//Going to get one functions for all the tabs. 
+//This is for testing purposes.
+
+function contactClick(){
+    $(".contact").show();
+    $(".resume").hide();
+    $(".work").hide();
+    $(".about").hide();
+}
+
+function resumeClick(){
+    $(".contact").hide();
+    $(".resume").show();
+    $(".work").hide();
+    $(".about").hide();
+}
+
+function aboutClick(){
+    $(".contact").hide();
+    $(".resume").hide();
+    $(".work").hide();
+    $(".about").show();
+}
+
+function FrontpageClick(){
+    $(".contact").hide();
+    $(".resume").hide();
+    $(".work").hide();
+    $(".about").hide();
+}
+
+function workClick(){
+    $(".contact").hide();
+    $(".resume").hide();
+    $(".work").show();
+    $(".about").hide();
+}
