d1f3070b3ca66344e0a55b2e69fb638c5be8e9ce
Jan Erik <janegge1990@gmail.com>
2018-05-19 19:00:52 +0200

    Update Apiget.js

--- Jquery Api/Apiget.js
+++ Jquery Api/Apiget.js
@@ -2,7 +2,7 @@ var openwatherappid = '3db348938434011b82239668b6240a1b';
 var openwatherurl = 'api.openweathermap.org/data/2.5/weather?q=London&appid=';
 
 
-    $.get('api.openweathermap.org/data/2.5/weather?q=London&appid=3db348938434011b82239668b6240a1b',        
+    $.get('api.openweathermap.org/data/2.5/weather?q=London&appid=',        
             function(data){
             console.log(data.name)
           });
